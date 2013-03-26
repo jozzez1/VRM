c16a59ec13e49034cff70a6f98e4eb981a8124b1
jozzez1 <jozze.zepl@hotmail.com>
2013-03-26 02:30:20 -0100

    rewrite of plotting script

--- Naloga04/plot4.sh
+++ Naloga04/plot4.sh
@@ -5,74 +5,112 @@ SAVE=$2
 MODE=$3
 NUM=$4
 
-# plot the F(beta)
-if [ ${MODE} -eq 0 ]; then
+# write stuff in my report ...
+function write ()
+{
+	dat=$1
+	report=$2
 
-	echo "reset;
-	set tics nomirror;
-	set autoscale y2;
-	set key top left;
-	set label \"N = $NUM\" at screen 0.72,0.3;
-	set y2tics;
-	set log y2;
-	set ylabel 'F';
-	set y2label 'Z';
-	plot \"$DAT.txt\" u 1:3 w l title 'F' axes x1y1;
-	replot \"$DAT.txt\" u 1:2 w l lt 3 title 'Z' axes x1y2" | gnuplot -p
-	
-	if [ ${SAVE} -eq 1 ]; then
-	
-		echo "reset;
-		set term epslatex color solid;
-		set output \"$DAT.tex\";
-		set tics nomirror;
-		set autoscale y2;
-		set key top left;
-		set y2tics;
-		set log y2;
-		set tics out;
-		set label \"N = $NUM\" at screen 0.72,0.3;
-		set title 'Prosta energija';
-		set xlabel '"'$\beta$'"';
-		set ylabel '"'$F$'"';
-		set y2label '"'$Z$'"';
-		plot \"$DAT.txt\" u 1:3 w l title '"'$F(\beta)$'"' axes x1y1;
-		replot \"$DAT.txt\" 1:2 w l lt 3 title '"'$Z(\beta)$'"' axes x1y2 "| gnuplot
-	
-		epstopdf $DAT.eps
-		PIC="joze_zobec_04.tex"
-	
-		echo >> $PIC
-		echo '\begin{figure}[H]'               >> $PIC
-		echo '   \begin{center}'               >> $PIC
-		echo '      \input{'"$DAT.tex"'}'      >> $PIC
-		echo '   \end{center}'                 >> $PIC
-		echo '   \vspace{-20pt}'               >> $PIC
-		echo '   \caption{Some caption ...}'   >> $PIC
-		echo '   \label{fig:pic}'              >> $PIC
-		echo '   \vspace{-10pt}'               >> $PIC
-		echo '\end{figure}'                    >> $PIC
-		echo >> $PIC
+	echo >> $report
+	echo >> $report
+	echo '\begin{figure}[H]'               >> $report
+	echo '   \begin{center}'               >> $report
+	echo '      \input{'"$dat.tex"'}'      >> $report
+	echo '   \end{center}'                 >> $report
+	echo '   \vspace{-20pt}'               >> $report
+	echo '   \caption{Some caption ...}'   >> $report
+	echo '   \label{fig:pic}'              >> $report
+	echo '   \vspace{-10pt}'               >> $report
+	echo '\end{figure}'                    >> $report
+	echo >> $report
+}
 
+function plot ()
+{
+	save=$1
+	mode=$2
+	dat=$3
+	num=$4
+
+	temp=scriptP.gp
+
+	rm -rf $temp
+	touch $temp
+
+	set pflag
+	set xaxis
+	set yaxis
+	set y2axis
+
+	if [ ${mode} -eq 0 ]
+	then
+		xaxis='$\\beta$'
+		yaxis='$F$'
+		y2axis='$Z$'
+	elif [ ${mode} -eq 1 ]
+	then
+		xaxis='$\beta$'
+		yaxis='$E$'
+	elif [ ${mode} -eq 2 ]
+	then
+		xaxis='$t$'
+		yaxis='$C$'
+	elif [ ${mode} -eq 3 ]
+	then
+		xaxis='$t$'
+		yaxis='$J$'
 	fi
 
-elif [ ${MODE} -eq 1 ]; then
+	# we prepare the terrain
+	echo >> $temp
+	echo 'reset' >> $temp
 
-	echo "reset;
-	set tics nomirror;
-	set autoscale y2;
-	set key top left;
-	set y2tics;
-	set ylabel 'Re (E)'
-	set y2label 'Im (E)'
-	set label \"N = $NUM\" at screen 0.72,0.3;
-	set log y2;
-	plot \"$DAT.txt\" u 1:4 w l title 'Re (E)' axes x1y1;
-	replot \"$DAT.txt\" u 1:2 w l lt 3 title 'Z' axes x1y2" | gnuplot -p
+	if [ ${save} -eq 1 ]
+	then
+		echo 'set term epslatex color solid' >> $temp
+		echo "set output \"$dat.tex\""       >> $temp
 
+	else
+		pflag="-p"
+	fi
+
+	if [ ${mode} -eq 0 ]; then
+
+		echo 'set tics nomirror'    >> $temp
+		echo 'set autoscale y2'     >> $temp
+		echo 'set y2tics'           >> $temp
+		echo 'set log y2'           >> $temp
+		echo "set y2labe '$y2axis'" >> $temp
+	fi
+
+	echo "set xlabel '$xaxis'"  >> $temp
+	echo "set ylabel '$yaxis'"  >> $temp
+	echo "set label 'N = $num' at screen 0.72,0.3" >> $temp
+
+	# here comes the plotting part
+	echo >> $temp
+	if [ ${mode} -eq 0 ]; then
+		echo "plot \"$dat.txt\" u 1:3 w l title '$yaxis' axes x1y1, \\" >> $temp
+		echo "\"$dat.txt\" u 1:2 w l lt 3 title '$y2axis' axes x1y2"    >> $temp
+
+	else
+		echo "plot \"$dat.txt\" u 1:4 w l title '$yaxis'"               >> $temp
+	fi
+
+	# we finish with the file ...
+	echo >> $temp
+
+	# ... so we start plotting ... ;)
+	cat $temp | gnuplot $pflag
+}
+
+plot 0 $MODE $DAT $NUM
+
+if [ ${SAVE} - eq 1 ]; then
+	plot $SAVE $MODE $DAT $NUM
+	epstopdf $DAT.eps
+	write $DAT joze_zobec_04.tex
 fi
-
-#rm -rf $DAT.txt
 
 exit 0
 
