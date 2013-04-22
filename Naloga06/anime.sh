0537b6d24e3d68a17d85f7942011ecb2ce4a2a99
Jozze <jozze.zepl@hotmail.com>
2013-04-22 02:14:14 +0200

    plotter fix

--- Naloga06/anime.sh
+++ Naloga06/anime.sh
@@ -21,7 +21,12 @@ cd $ANI
 
 for FILE in `ls $DIR | grep txt`;
 do
-	echo "set terminal jpeg enhanced font \"/usr/local/lib/X11/fonts/TTF/luximr.ttf,12\" size 550,400;
+	I="$COUNT"
+	T=`echo "scale = 4; $dT * $I + 0.5" | bc`
+
+	echo "T = $T"
+
+	echo "set terminal jpeg enhanced font \"/usr/local/lib/X11/fonts/ChromeOS/Arimo-Regular.ttf,12\" size 550,400;
 	  unset surf;
 	  set view map;
 	  set palette rgbformulae 10,10,10 maxcolor 2;
@@ -32,8 +37,6 @@ do
 
 	(( COUNT ++ ))
 
-	I="$COUNT"
-	T=`echo "scale=5;$dT*$I" | bc`
 done
 
 mencoder -msgcolor "mf://*.jpg" -mf w=600:h=400:fps=${FPS}:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o $1.avi
