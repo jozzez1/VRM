df7d4ba838de5f95988549cfdb506ce81951f59c
Jozze <jozze.zepl@hotmail.com>
2014-01-07 01:24:03 +0100

    added the Suzuki-Trotter time/temperature propagation

--- Naloga09/matrixB.m
+++ Naloga09/matrixB.m
@@ -5,15 +5,22 @@ function K = matrixB (B, L, k, j, b, N)
 
 	U = matrixU (b);
 
-	n = dec2bin(k-1, 2*N)(1,j:j+1);
+	n (1,1) = dec2bin(k-1, 2*N)(1,j);
+	n (1,2) = dec2bin(k-1, 2*N)(1, mod(j,2*N) + 1);
 	x = U (bin2dec(n)+1,:);
+
+	if j <= 2*N - 1
+		lambda = L{j};
+	elseif j == 2*N
+		lambda = 1;
+	endif
 
 	K = 0;
 	for i = 1:4
 		s1 = bin2dec(dec2bin (i-1,2)(1,1)) + 1;
 		s2 = bin2dec(dec2bin (i-1,2)(1,2)) + 1;
 
-		K += x(i) * B{s1, j} * L{j} * B{s2,j+1};
+		K += x(i) * B{s1, j} * lambda * B{s2,mod(j, 2*N) + 1};
 	end
 
 	return;
