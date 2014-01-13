04e225adf64dd631ece383f2e0d8fc073fb489d5
Jozze <jozze.zepl@hotmail.com>
2014-01-13 15:24:56 +0100

    forgot to delete this useless crap

--- Naloga09/energy_step.m
+++
@@ -1,9 +0,0 @@
-function [Bnew, Lnew] = energy_step (B, L, b, N)
-
-	% Suzuki-Trotter
-	[Bnew, Lnew] = part_step (B, L, 0.5 * b, N, 'odd');
-	[Bnew, Lnew] = part_step (Bnew, Lnew, b, N, 'even');
-	[Bnew, Lnew] = part_step (Bnew, Lnew, 0.5 * b, N, 'odd');
-	
-	return;
-endfunction
