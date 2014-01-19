02b126b5907b946b88d1b3d0ff335633be6eb21f
Jože Zobec <jozze@graylight.(none)>
2014-01-19 22:36:15 +0100

    working very well

--- Naloga09/DFT.m
+++ Naloga09/DFT.m
@@ -1,11 +1,12 @@
-function [E, u, U] = DFT (rmin, rmax, N)
+function [E0, E, u, U] = DFT (rmin, rmax, N, prec)
 	% we initialize our starting vectors
 	[E, u, U] = start (-0.5, rmin, rmax, N);
 
 	% now we iterate it
-	[E, u, U] = DFT_converge (E, u, U, rmin, rmax);
+	[E, u, U] = DFT_converge (E, u, U, rmin, rmax, prec);
 
 	% well, now we could integrate it ... should we?
+	E0 = energy (E, u, U, rmin, rmax, N);
 
 	return;
 	
--- Naloga09/DFT_converge.m
+++ Naloga09/DFT_converge.m
@@ -1,10 +1,10 @@
-function [E2, u2, U2] = DFT_converge (E, u, U, rmin, rmax)
+function [E2, u2, U2] = DFT_converge (E, u, U, rmin, rmax, prec)
 	E1 = E;
 	u1 = u;
 	U1 = U;
 
 	[E2, u2, U2] = DFT_step (E1, u1, U1, rmin, rmax);
-	while (sumsq(u2 - u1) + sumsq(U2 - U1) > 1e-6)
+	while (sumsq(u2 - u1) + sumsq(U2 - U1) > prec)
 		E1 = E2;
 		u1 = u2;
 		U1 = U2;
